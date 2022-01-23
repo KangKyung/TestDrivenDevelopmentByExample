a1b4875519e4b3e4974b84ed61c8be4eec7d9d23
kangkyung <rudgns0508@naver.com>
2022-01-23 20:47:26 +0900

    Chore: Project Setting

---
+++ TDDExampleCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDDExampleCode
+//
+//  Created by 강경 on 2022/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TDDExampleCode/TDDExampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDExampleCodeApp.swift
+//  TDDExampleCode
+//
+//  Created by 강경 on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TDDExampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
